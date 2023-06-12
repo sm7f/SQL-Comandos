1f51aa37db4c6a2d363c3abfeb87b6f044f10c6c
Herberth Amorim <86411631+sm7f@users.noreply.github.com>
2023-06-12 15:51:32 -0300

    .

--- AcrescentaVenda.sql
+++ AcrescentaVenda.sql
@@ -1,5 +1,12 @@
 Select ValorTotalVenda IdentificacaoClienteVenda, SiglaTipoDocFiscal from VENDA
 
-Select * from VENDA where DataHoraVenda = '2023-02-16 15:54:19.183'
+Select * from VENDA where DataHoraVenda = '2023-06-12 13:48:06.760'
 
 Select * from VENDA
+
+
+# Quando a venda some do caixa
+
+Select * from VENDA where cdvenda = '19673'
+
+Update VENDA set SiglaStatusVenda = 'VF' where cdvenda = '19673'
