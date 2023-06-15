b86856ba1cb282a05b18f24c8f9f6f949731ff73
Herberth Amorim <86411631+sm7f@users.noreply.github.com>
2023-06-15 09:15:47 -0300

    ,

--- AcrescentaVenda.sql
+++ AcrescentaVenda.sql
@@ -5,8 +5,3 @@ Select * from VENDA where DataHoraVenda = '2023-06-12 13:48:06.760'
 Select * from VENDA
 
 
-# Quando a venda some do caixa
-
-Select * from VENDA where cdvenda = '19673'
-
-Update VENDA set SiglaStatusVenda = 'VF' where cdvenda = '19673'
---
+++ SumiuVendaDoCaixa.sql
@@ -0,0 +1,5 @@
+# Quando a venda some do caixa
+
+Select * from VENDA where cdvenda = '19673'
+
+Update VENDA set SiglaStatusVenda = 'VF' where cdvenda = '19673'
