9206afbf2a914e8ed8b698a873375b0d3b0867ae
Herberth Amorim <86411631+sm7f@users.noreply.github.com>
2023-08-17 11:19:19 -0300

    .

--- TransformarRejeitada.sql
+++ TransformarRejeitada.sql
@@ -1,2 +1,6 @@
 update nfe set StatusContingencia=1 where Id='53230703778352000160901080000001159232533892'
 update nfe set StatusAndamento='NN' where Id='53230703778352000160901080000001159232533892'
+
+
+Transformar em Cancelada
+update nfe set StatusCancelada='1' where Id='53230802408820000142901080000000961347140440'
