96cdd7dcf12537f386f46b84ddad32c223fe65cc
Herberth Amorim <86411631+sm7f@users.noreply.github.com>
2023-12-11 12:40:25 -0300

    .

--- travouIntegrador.sql
+++ travouIntegrador.sql
@@ -1,4 +1,11 @@
 use polisystemserversqldb
+go
+
 update caixa set statusexportacao = 0
 go
+
 update venda set statusexportacao = 0
+go
+
+update caixa set statusexportacao = 1 where datacaixa >= '01-12-2023'
+go
