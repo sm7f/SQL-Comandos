16e71d64a8c02152088ccfeefd364a0e2fc4f753
Herberth Amorim <86411631+sm7f@users.noreply.github.com>
2024-01-25 13:54:46 -0300

    .

---
+++ FilialConfigErrado.sql
@@ -0,0 +1,13 @@
+select cdempresa, cdfuncionario from funcionario_empresa
+
+select cdempresa from config_geral_sys
+
+select cdempresa, nomeempresa from empresa
+
+select cdempresa,cnpjempresa,identificacaoempresa from empresa
+
+delete from cdempresa where cdempresa = 4
+
+insert into funcionario_empresa (cdempresa,cdfuncionario) values (8,1)
+
+in
