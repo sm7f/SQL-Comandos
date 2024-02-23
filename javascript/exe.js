b1db701cdc113bf646531301fe2042cb7d7cd144
Herberth Amorim <86411631+sm7f@users.noreply.github.com>
2024-02-23 17:57:30 -0300

    .

--- javascript/exe.js
+++ javascript/exe.js
@@ -4,31 +4,3 @@ console.log(applyModule.applyValidBool(1))
 console.log(applyModule.applyValidBool(-1))
 console.log(applyModule.applyValidBool(0))
 
-const characterHead = {
-    typeHead:function(){
-        const hModel = ['Cabelo','orelha','olhos','nariz','boca']
-        return hModel
-    }
-}
-const characterBody = {
-    typeHead:function(){
-        const hModel = ['Cabelo','orelha','olhos','nariz','boca']
-        return hModel
-    }
-}
-const characterArms = {
-    typeHead:function(){
-        const hModel = ['Cabelo','orelha','olhos','nariz','boca']
-        return hModel
-    }
-}
-const characterLegs = {
-    typeHead:function(){
-        const hModel = ['Cabelo','orelha','olhos','nariz','boca']
-        return hModel
-    }
-}
-
-
-console.log(character.typeHead())
-
