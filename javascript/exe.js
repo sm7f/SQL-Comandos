d6dff47d798698a38d18779a9c8199c18be237c9
Herberth Amorim <86411631+sm7f@users.noreply.github.com>
2024-02-23 17:50:37 -0300

    .

--- javascript/exe.js
+++ javascript/exe.js
@@ -3,3 +3,43 @@ const applyModule = require('../javascript/brincando')
 console.log(applyModule.applyValidBool(1))
 console.log(applyModule.applyValidBool(-1))
 console.log(applyModule.applyValidBool(0))
+
+const characterHead = {
+    typeHead:function(){
+        const hModel = ['Cabelo','orelha','olhos','nariz','boca']
+        return hModel
+    }
+}
+const characterBody = {
+    typeHead:function(){
+        const hModel = ['Cabelo','orelha','olhos','nariz','boca']
+        return hModel
+    }
+}
+const characterArms = {
+    typeHead:function(){
+        const hModel = ['Cabelo','orelha','olhos','nariz','boca']
+        return hModel
+    }
+}
+const characterLegs = {
+    typeHead:function(){
+        const hModel = ['Cabelo','orelha','olhos','nariz','boca']
+        return hModel
+    }
+}
+const characterFoot = {
+    typeHead:function(){
+        const hModel = ['Cabelo','orelha','olhos','nariz','boca']
+        return hModel
+    }
+}
+const characterFoot = {
+    typeHead:function(){
+        const hModel = ['Cabelo','orelha','olhos','nariz','boca']
+        return hModel
+    }
+}
+
+console.log(character.typeHead())
+
