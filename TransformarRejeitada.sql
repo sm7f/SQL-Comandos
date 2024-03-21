bd7cad80a7876a3ed1b0647a3fbe4b70e00af7a8
Herberth Amorim <86411631+sm7f@users.noreply.github.com>
2024-03-21 11:19:14 -0300

    .

--- TransformarRejeitada.sql
+++ TransformarRejeitada.sql
@@ -4,13 +4,14 @@ update nfe set StatusAndamento='NN' where Id='5323110514314700015455001000001751
 
 
 Transformar em Cancelada
-update nfe set StatusCancelada='1' where Id='53240309464896000134550010000014001387201450'
+update nfe set StatusCancelada='1' where Id='53240307765103000353550020000001041345614430'
 update nfe set StatusCancelada='1' where Id='53230811960064000150901080000000581010685645'
 
-Transformar em Aprovada
+Transformar em Aprovada-
 
 use polisystemserversqldb
 go
 update nfe set statusaprovada = 1 where id = '53240207233443000171901080000006469018937643'
 go
 
+-
