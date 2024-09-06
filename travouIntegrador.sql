e4682b28b1427fda4eeb428a3435aff76c01bd88
Herberth Amorim <86411631+sm7f@users.noreply.github.com>
2024-09-06 08:15:34 -0300

    .

--- travouIntegrador.sql
+++ travouIntegrador.sql
@@ -4,7 +4,7 @@ go
 update caixa set statusexportacao = 0
 go
 
-update venda set statusexportacao = 0
+update venda set statusexportacao = 1
 go
 
 update caixa set statusexportacao = 1 where datacaixa >= '01-12-2023'
