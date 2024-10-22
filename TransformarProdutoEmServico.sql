3bbae8a3731796b3c5bce412f21a10ba5f0f314a
Herberth Amorim <86411631+sm7f@users.noreply.github.com>
2024-10-22 12:12:45 -0300

    .

---
+++ TransformarProdutoEmServico.sql
@@ -0,0 +1,3 @@
+select [CdProduto],StatusTipoProduto,IndIncentivoFiscalISSQN,IndExibilidadeISSQN from PRODUTO where CodigoNCMProduto = '00000000'
+
+update PRODUTO set StatusTipoProduto='SR',IndIncentivoFiscalISSQN = 2, IndExibilidadeISSQN = 1 where CodigoNCMProduto = '00000000'
