4c818b68963507baee7044135b6438e33933381e
Herberth Amorim <86411631+sm7f@users.noreply.github.com>
2024-11-01 11:47:23 -0300

    .

---
+++ SincronizaVendasPDV.sql
@@ -0,0 +1,10 @@
+update dbo."TB_DocumentoFiscal" set "DataSincronizacao" = '2024-09-01 13:09:02.605'
+where "DataAlteracao" >= '2024-09-30' and "DataAlteracao" <= '2024-11-01'
+ 
+ 
+update dbo."TB_Caixa" set "DataSincronizacao" = '2024-09-01 13:09:02.605'
+where "DataAlteracao" >= '2024-09-30' and "DataAlteracao" <= '2024-11-01'
+ 
+ 
+update dbo."TB_Venda" set "DataSincronizacao" = '2024-09-01 13:09:02.605'
+where "DataAlteracao" >= '2024-09-30' and "DataAlteracao" <= '2024-11-01'
