560c6a1148782f4fa252408f8933e0a8765608b5
Herberth Amorim <86411631+sm7f@users.noreply.github.com>
2025-01-27 17:22:44 -0300

    .

--- BuscaVendas.sql
+++ BuscaVendas.sql
@@ -1,6 +1,24 @@
 SELECT cdvenda, DataHoraVenda, valortotalvenda
 FROM VENDA WHERE DataHoraVenda > '2025-01-22' 
 
+//Venda dia *//
+SELECT cdvenda, DataHoraVenda, valortotalvenda
+FROM VENDA
+WHERE DataHoraVenda > CAST(GETDATE() AS DATE)
+
+//Venda Mes *//
+SELECT cdvenda, DataHoraVenda, valortotalvenda
+FROM VENDA
+WHERE YEAR(DataHoraVenda) = YEAR(GETDATE()) 
+  AND MONTH(DataHoraVenda) = MONTH(GETDATE())
+
+
+//Mês anterior *//
+SELECT cdvenda, DataHoraVenda, valortotalvenda
+FROM VENDA
+WHERE YEAR(DataHoraVenda) = YEAR(DATEADD(MONTH, -1, GETDATE()))
+  AND MONTH(DataHoraVenda) = MONTH(DATEADD(MONTH, -1, GETDATE()))
+
 
 // Altera Status Venda *//
 update VENDA 
