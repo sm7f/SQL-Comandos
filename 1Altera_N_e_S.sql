226234543368846a1cbda009d33ceb31d9b2f2ce
Herberth Amorim <86411631+sm7f@users.noreply.github.com>
2025-03-03 12:36:27 -0300

    .

--- 1Altera_N_e_S.sql
+++ 1Altera_N_e_S.sql
@@ -1,21 +1,18 @@
 // Seleciona o Dia //
-SELECT cdvenda, DataHoraVenda, valortotalvenda
-FROM VENDA WHERE DataHoraVenda > '2025-01-22' 
+SELECT cdvenda, DataHoraVenda, valortotalvenda, IdentificacaoClienteVenda, SiglaTipoDocFiscal, SiglaStatusVenda, NrDocFiscalVendaFROM VENDA WHERE DataHoraVenda > '2024-05-22' 
+FROM VENDA WHERE DataHoraVenda > '2024-05-22' 
 
 //Venda dia *//
-SELECT cdvenda, DataHoraVenda, valortotalvenda
-FROM VENDA
+SELECT cdvenda, DataHoraVenda, valortotalvenda, IdentificacaoClienteVenda, SiglaTipoDocFiscal, SiglaStatusVenda, NrDocFiscalVendaFROM VENDA
 WHERE DataHoraVenda > CAST(GETDATE() AS DATE)
 
 //Venda Mes *//
-SELECT cdvenda, DataHoraVenda, valortotalvenda
-FROM VENDA
+SELECT cdvenda, DataHoraVenda, valortotalvenda, IdentificacaoClienteVenda, SiglaTipoDocFiscal, SiglaStatusVenda, NrDocFiscalVendaFROM VENDA
 WHERE YEAR(DataHoraVenda) = YEAR(GETDATE()) 
   AND MONTH(DataHoraVenda) = MONTH(GETDATE())
 
 //Mês anterior *//
-SELECT cdvenda, DataHoraVenda, valortotalvenda
-FROM VENDA
+SELECT cdvenda, DataHoraVenda, valortotalvenda, IdentificacaoClienteVenda, SiglaTipoDocFiscal, SiglaStatusVenda, NrDocFiscalVendaFROM VENDA
 WHERE YEAR(DataHoraVenda) = YEAR(DATEADD(MONTH, -1, GETDATE()))
   AND MONTH(DataHoraVenda) = MONTH(DATEADD(MONTH, -1, GETDATE()))
 
@@ -27,5 +24,5 @@ where CdVenda = '50178'
 
 
 // Altera NrFiscal Venda *//
-update VENDA set NrDocFiscalVenda = '1569' where CdVenda = '8663'
+update VENDA set NrDocFiscalVenda = '726' where CdVenda = '1573'
 update VENDA set NrDocFiscalVenda = '324' where CdVenda = '376'
